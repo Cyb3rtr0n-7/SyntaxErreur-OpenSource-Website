fbc01cc56b74b4712996a23a3910a68c5903fa30
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2023-04-08 18:32:35 +0530

    add scroll line

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 // components
 import Marquee from "@components/Marquee";
 
 export default function Home() {
+  const [height, setHeight] = useState(0); // initial height of div is 100px
+
+  useEffect(() => {
+    function handleScroll() {
+      const scrollPosition = window.scrollY;
+      setHeight(0 + scrollPosition); // increase height by scroll position
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="home">
       <div className="home__marquee">
@@ -15,6 +30,13 @@ export default function Home() {
             <h1>Syntax Erreur</h1>
           </div>
         </div>
+
+        <div
+          className="home__container__scrollLine home__container__scrollLine1"
+          style={{
+            height: `${height}px`,
+          }}
+        ></div>
       </div>
     </div>
   );
